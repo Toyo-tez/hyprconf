bf3f2af684852cc6fa646267f902271d43c77476
Toyo <Toyo-tez@users.noreply.github.com>
2026-06-22 17:48:45 -0300

    Add packages, more config, etc

---
+++ packages.sh
@@ -0,0 +1,3 @@
+sudo pacman -S rofi network-manager-applet swayosd hyprpaper syncthing waybar
+flatpak install io.github.milkshiift.GoofCord
+curl -fsS https://dl.brave.com/install.sh | FLAVOR=origin CHANNEL=nightly sh
