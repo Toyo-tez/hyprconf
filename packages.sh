667302cd2d9a0e3e208223642e17934e6a5440a5
Toyo <Toyo-tez@users.noreply.github.com>
2026-06-29 20:32:40 -0300

    Add hyprdinamicmonitors

--- packages.sh
+++ packages.sh
@@ -1,3 +1,4 @@
 sudo pacman -S rofi network-manager-applet swayosd hyprpaper syncthing waybar
+shelly aur install hyprdinamicmonitors-bin
 flatpak install io.github.milkshiift.GoofCord
 curl -fsS https://dl.brave.com/install.sh | FLAVOR=origin CHANNEL=nightly sh
